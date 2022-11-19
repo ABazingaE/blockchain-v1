2c8232fa0e78aead600f36d41ff8869b6dc0ba2d
bazinga <1065564624@qq.com>
2022-11-19 22:45:35 +0800

    实现<生成“工作量证明”>方法

--- proofofwork.go
+++ proofofwork.go
@@ -9,3 +9,26 @@ type ProofOfWork struct {
 	//目标值,使用该类型是因为提供了多种方法：比较、将哈希值转换为big.int
 	target big.Int
 }
+
+/**
+ * @description:新生成工作量证明,block由用户提供，target由系统提供
+ * @param {*Block} block
+ * @return {*ProofOfWork}
+ */
+func NewProofOfWork(block *Block) *ProofOfWork {
+	pow := ProofOfWork{
+		block: block,
+	}
+
+	//难度暂时写死，后续更新推导方式
+	tempStr := "0000100000000000000000000000000000000000000000000000000000000000"
+
+	tempBigInt := new(big.Int)
+
+	tempBigInt.SetString(tempStr, 16)
+
+	pow.target = *tempBigInt
+
+	return &pow
+
+}
