b83b75a20bb921d1dcf6da4e6aba90a8a6100c3b
bazinga <1065564624@qq.com>
2022-11-20 17:26:38 +0800

    结合bolt改写“NewBlockChain”方法

--- blockchain.go
+++ blockchain.go
@@ -1,27 +1,75 @@
 package main
 
+import "github.com/boltdb/bolt"
+
 /*
 	定义区块链结构（数组模拟）
 */
 type BlockChain struct {
-	Blocks []*Block
+	//用于存储数据
+	db *bolt.DB
+
+	//最后一个区块的哈希值
+	tail []byte
 }
 
 /*
 	创建区块链
-	1.创建BlockChain
-	2.添加一个创世区块
+	1.查找是否有数据库，若有则填充blockchain返回，若无则新建数据库，且添加创世区块并返回
 */
 //创世语
 const genesisInfo = "Hello,Web3!"
+const blockchainDbFile = "blockchain"
+const blockBucket = "blockBucket"
+const lastblockKey = "lastBlockKey"
 
-func NewBlockChain() *BlockChain {
+func NewBlockChain() (*BlockChain, error) {
 
-	genesisBlock := NewBlock(genesisInfo, nil)
-	bc := BlockChain{
-		Blocks: []*Block{genesisBlock},
+	// genesisBlock := NewBlock(genesisInfo, nil)
+	// bc := BlockChain{
+	// 	Blocks: []*Block{genesisBlock},
+	// }
+	// return &bc
+
+	var lastHash []byte
+
+	db, err := bolt.Open(blockchainDbFile, 0600, nil)
+	if err != nil {
+		return nil, err
 	}
-	return &bc
+
+	db.Update(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(blockBucket))
+
+		//若没有bucket，则创建，添加创世块
+		if bucket == nil {
+			//create bucket
+			bucket, err = tx.CreateBucket([]byte(blockBucket))
+			if err != nil {
+				return err
+			}
+
+			//create genisisBlock
+			genisisBlock := NewBlock(genesisInfo, nil)
+
+			//put into the bucket,key is the hash of the block, value is the block
+			bucket.Put(genisisBlock.Hash, genisisBlock.serialize)
+			bucket.Put([]byte(lastblockKey), genisisBlock.Hash)
+		}
+
+		lastHash = bucket.Get([]byte(lastblockKey))
+
+		return nil
+
+	})
+
+	blockchain := BlockChain{
+		db:   db,
+		tail: lastHash,
+	}
+
+	return &blockchain, nil
+
 }
 
 /*
